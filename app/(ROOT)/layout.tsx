cd59bc1e4230658e5e18a7b612288717bf5e9955
Zahran03 <zalfarizi502@gmail.com>
2024-12-04 17:02:03 +0700

    feat : create logic for login

--- app/(ROOT)/layout.tsx
+++ app/(ROOT)/layout.tsx
@@ -1,7 +1,13 @@
 import { ReactNode } from "react";
+import Navbar from "../components/Navbar";
 
 export default function Layout({
   children,
-}: Readonly<{ children: ReactNode }>) {
-  return <main className="font-work-sans">{children}</main>;
+}: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <main className="font-work-sans">
+      <Navbar />
+      {children}
+    </main>
+  );
 }
--- auth.ts
+++ auth.ts
@@ -1,11 +1,8 @@
-import NextAuth from "next-auth";
+
 import GitHub from "next-auth/providers/github";
+import NextAuth from "next-auth";
+
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    GitHub({
-      clientId: process.env.GITHUB_CLIENT_ID as string,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-    }),
-  ],
+  providers: [GitHub]
 });
